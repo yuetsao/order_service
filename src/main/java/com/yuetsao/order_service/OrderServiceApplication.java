f90d31676ae03707ad5b7482326f6f2f8f7800e7
caoyue <yuetsao@foxmail.com>
2018-12-27 12:04:26 +0800

    熔断报警

--- src/main/java/com/yuetsao/order_service/OrderServiceApplication.java
+++ src/main/java/com/yuetsao/order_service/OrderServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
--- src/main/java/com/yuetsao/order_service/controller/OrderController.java
+++ src/main/java/com/yuetsao/order_service/controller/OrderController.java
@@ -2,13 +2,17 @@ package com.yuetsao.order_service.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.yuetsao.order_service.service.ProductOrderService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -17,9 +21,13 @@ public class OrderController {
     @Autowired
     private ProductOrderService productOrderService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @RequestMapping("save")
     @HystrixCommand(fallbackMethod = "saveOrderFail")
-    public Object save(@RequestParam("user_id")int userId, @RequestParam("product_id") int productId) {
+    public Object save(@RequestParam("user_id")int userId, @RequestParam("product_id") int productId, HttpServletRequest request) {
+
         Map<String, Object> result = new HashMap<>();
         result.put("code" , -1);
         result.put("msg", "下单成功");
@@ -27,7 +35,20 @@ public class OrderController {
         return result;
     }
 
-    private Object saveOrderFail(int userId, int productId) {
+    private Object saveOrderFail(int userId, int productId, HttpServletRequest request) {
+        //监控报警
+        String saveOrderKey = "save-order";
+        String sendValue = stringRedisTemplate.opsForValue().get(saveOrderKey);
+        String ip = request.getRemoteAddr();
+        new Thread(()->{
+            if(StringUtils.isBlank(sendValue)) {
+                System.out.println("紧急短信，用户下单失败"+ip);
+                //发送一个http请求，调用短信服务
+                stringRedisTemplate.opsForValue().set(saveOrderKey, "saveOrderFail", 20, TimeUnit.SECONDS);
+            }else {
+                System.out.println("已经发送短信，二十分钟内不发送");
+            }
+        }).start();
         Map<String, Object> result = new HashMap<>();
         result.put("code" , -1);
         result.put("msg", "目前下单人数过多，请稍后重试");
